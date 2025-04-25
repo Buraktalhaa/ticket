0c9b974b0f523c61688bbe03ba8c91c0357881c4
Buraktalhaa <talha45879@gmail.com>
2025-04-25 15:47:48 +0300

    refrestToken process folder created

---
+++ backend/src/auth/controllers/refresh.ts
@@ -0,0 +1,46 @@
+import prisma from '../../common/utils/prisma';
+import { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { createToken } from "../utils/createToken";
+
+
+export async function refreshController(req: Request, res: Response) {
+    const refreshToken = req.body.refreshToken
+    console.log("Refresh token in refresh",refreshToken)
+
+    if (!refreshToken) {
+        res.status(401).json({ message: 'Refresh token is missing' });
+        return
+    }
+
+    try {
+        const payload:any = jwt.verify(refreshToken, process.env.REFRESH_SECRET!);
+        console.log(payload)
+        const accessToken = createToken(payload.userId, payload.email, process.env.ACCESS_SECRET!, 10 * 60)
+        console.log("access token =>",accessToken)
+        await prisma.token.update({
+            where: {
+                email:payload.email
+            },
+            data: {
+                accessToken: accessToken
+            }
+        })
+        res.status(200).json({
+            status: 'success',
+            message: 'Access token sent successful',
+            accessToken,
+            data: {
+                email:payload.email,
+            }
+        });
+
+    } catch (error: any) {
+        if (error.name === 'TokenExpiredError') {
+            res.status(401).json({ message: 'Refresh token has expired' });
+            // Tekrar sign in olmali
+        } else {
+            res.status(403).json({ message: 'Invalid refresh token' });
+        }
+    }
+}
