4fe024bc820a3761c3c7d7fa30bf27252c0a54fc
Buraktalhaa <talha45879@gmail.com>
2025-04-27 22:09:43 +0300

    updatePassword added in profile.routes

--- backend/src/profile/routers/profile.routes.ts
+++ backend/src/profile/routers/profile.routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { authenticateToken } from '../middleware/tokenController'
 import { getProfile } from "../controller/getProfile";
 import { updateProfile } from "../controller/updateProfile";
+import { updatePassword } from "../controller/updatePassword";
 
 const router = Router()
 
 router.get('/myProfile', authenticateToken, getProfile)
 
 router.put('/myProfile/update', authenticateToken , updateProfile)
+
+router.put('/myProfile/updatePassword', authenticateToken, updatePassword)
 
 export default router;
