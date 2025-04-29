e20db509dab48ed79ab95d5a0bd58aa47f78eb30
Buraktalhaa <talha45879@gmail.com>
2025-04-29 09:18:08 +0300

    forgotPassword created

---
+++ backend/src/auth/controllers/forgot-password.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { handleError } from "../../common/error-handling/handleError";
+import { Email } from "../../common/utils/email";
+import prisma from "../../common/utils/prisma";
+import { createToken } from "../utils/createToken";
+
+export async function forgotPasswordController(req: Request, res: Response) {
+    try {
+        const { email } = req.body;
+
+        const user = await prisma.user.findUnique({where: {
+            email
+        }})
+
+        if (!user) {
+            handleError(res,'Auth not found', 400)
+            return
+        }
+
+        const token = createToken(user.id, email, process.env.ACCESS_SECRET!, 10 * 2)
+
+        const oldToken = await prisma.passwordResetToken.findUnique({
+            where:{
+                userId:user.id
+            }
+        })
+
+        // if token exists 
+        if(oldToken){
+            if(oldToken.expiresAt < new Date()){
+                await prisma.passwordResetToken.update({
+                    where:{
+                        userId:user.id
+                    },
+                    data:{
+                        token,
+                        userId:user.id,
+                        expiresAt: new Date(Date.now() + 10 * 60 * 1000)
+                    }
+                })
+            }
+            else{
+                handleError(res,'Clicked in 10 minutes', 400)
+                return;
+            }
+        }
+
+        // Save token in db
+        await prisma.passwordResetToken.create({
+            data:{
+                token,
+                userId:user.id,
+                expiresAt: new Date(Date.now() + 10 * 60 * 1000)
+            }
+        })
+
+        const mail = new Email({ email, firstName: user.name }, `http://localhost:3000/auth/reset-password/${token}`);
+        await mail.send('Subject','forgot Password?');
+
+        res.status(200).json({
+            message: "Welcome email sent successfully",
+            resetToken: token
+        });
+        return;
+    }
+    catch (error) {
+        handleError(res, "Failed to send email", 500)
+        console.error(error);
+        return;
+    }
+}
