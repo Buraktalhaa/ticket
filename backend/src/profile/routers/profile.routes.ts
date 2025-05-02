207b0e00a614eebbfe374379288bd0f8c0dd9a2b
Buraktalhaa <talha45879@gmail.com>
2025-05-02 16:17:19 +0300

    profile routes updated

--- backend/src/profile/routers/profile.routes.ts
+++ backend/src/profile/routers/profile.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { authenticateToken } from '../middleware/tokenController'
+import { authenticateToken } from '../../common/middleware/tokenMiddleware'
 import { getProfile } from "../controller/getProfile";
 import { updateProfile } from "../controller/updateProfile";
 import { editPassword } from "../controller/editPassword";
-import { permissionControl } from "../middleware/permissionControl";
+import { permissionControl } from "../middleware/permissionMiddleware";
+import fileUploadMiddleware from "../../common/middleware/file-upload";
 
 const router = Router()
 
 router.get('/myProfile', authenticateToken, permissionControl , getProfile)
 
-router.put('/myProfile/update', authenticateToken ,permissionControl, updateProfile)
+router.put('/myProfile/update', authenticateToken ,permissionControl, fileUploadMiddleware('photoName'), updateProfile)
 
 router.put('/myProfile/updatePassword', authenticateToken, permissionControl, editPassword)
 
