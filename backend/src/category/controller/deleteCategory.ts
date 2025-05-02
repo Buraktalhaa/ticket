b6c5ab355b9d0059bd425df1079153040c8269b6
Buraktalhaa <talha45879@gmail.com>
2025-05-02 16:09:15 +0300

    deleteCategory created

---
+++ backend/src/category/controller/deleteCategory.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import prisma from "../../common/utils/prisma";
+import { handleError } from "../../common/error-handling/handleError";
+import { DecodedUser } from "../../common/type/request.type";
+import { ResponseStatus } from "../../common/enums/status.enum";
+
+export async function deleteCategory(req:Request, res:Response){
+    const {userId, email} = req.user as DecodedUser;
+    const categoryName = req.body.description
+
+    const category = await prisma.category.findUnique({
+        where:{
+            name:categoryName
+        }
+    })
+
+    if(!category){
+        handleError(res, "There is no such category", 400)
+        return;
+    }
+
+    await prisma.category.delete({
+        where: {
+            id: category.id
+        }
+    });
+
+    res.status(200).json({
+        status: ResponseStatus.SUCCESS,
+        message: 'Category deleted succesfully',
+    });
+    return;
+
+}
