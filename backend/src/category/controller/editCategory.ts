2fa72f8ea35fa3839bbffa77e02ebcb7b2e07803
Buraktalhaa <talha45879@gmail.com>
2025-05-02 16:17:33 +0300

    editCategory created

---
+++ backend/src/category/controller/editCategory.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import prisma from "../../common/utils/prisma";
+import { handleError } from "../../common/error-handling/handleError";
+import { ResponseStatus } from "../../common/enums/status.enum";
+
+export async function editCategory(req:Request, res:Response){
+    const {categoryName, newCategoryName } = req.body
+
+    const category = await prisma.category.findUnique({
+        where:{
+            name:categoryName
+        }
+    })
+
+    if(!category){
+        handleError(res,`Category doesn't exists: ${categoryName}`, 400)
+        return;
+    }
+
+    const newCategory = await prisma.category.update({
+        where:{
+            id:category.id
+        },
+        data:{
+            name:newCategoryName
+        }
+    })
+
+    res.status(200).json({
+        status: ResponseStatus.SUCCESS,
+        message: 'Category edited succesfully',
+    });
+    return;
+}
