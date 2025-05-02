edbf81e11a6d6e5fbe2a1d9244359ef8b9a0d9f6
Buraktalhaa <talha45879@gmail.com>
2025-05-02 16:07:20 +0300

    getProfile updated

--- backend/src/profile/controller/getProfile.ts
+++ backend/src/profile/controller/getProfile.ts
@@ -2,17 +2,18 @@ import { Request, Response } from 'express';
 import prisma from '../../common/utils/prisma';
 import { ResponseStatus } from '../../common/enums/status.enum';
 import { handleError } from '../../common/error-handling/handleError';
+import { DecodedUser } from '../../common/type/request.type';
+
 
 
 export async function getProfile(req:Request, res: Response) {
     try {
-        const decoded = req.user;
-        const email = decoded?.email
+        const {userId , email} = req.user as DecodedUser;
 
         // Find user in db
         const user = await prisma.user.findUnique({
             where: {
-                email
+                id:userId
             },
             include:{
 
