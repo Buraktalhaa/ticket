8049acdcaf8cc8f5ece1fb573dd3c599ffb6e3c2
Buraktalhaa <talha45879@gmail.com>
2025-05-04 14:17:24 +0300

    edit-ticket updated

--- backend/src/ticket/controller/editTicket.ts
+++ backend/src/ticket/controller/editTicket.ts
@@ -5,49 +5,39 @@ import { handleError } from "../../common/error-handling/handleError";
 import { ResponseStatus } from "../../common/enums/status.enum";
 
 
-export async function editTicket(req:Request, res:Response){
-    const {userId, email} = req.user as DecodedUser;
-    const { id, hour, day, categoryName, description, stock, sold, newDay, newHour } = req.body;
-
-    const category = await prisma.category.findUnique({
-        where:{
-            name:categoryName
-        }
-    })
-
-    if(!category){
-        handleError(res,`Category doesn't exists: ${categoryName}`, 400)
-        return;
-    }
-
+export async function editTicket(req: Request, res: Response) {
+    const { userId, email } = req.user as DecodedUser;
+    const { id, ...restData } = req.body;
 
     const ticket = await prisma.ticket.findFirst({
-        where:{
+        where: {
+            id,
             userId,
-            categoryId:category.id,
-            hour,
-            day
         }
-    })
-    if(!ticket){
+    });
+
+
+    if (!ticket) {
         handleError(res, "There is no such ticket", 400)
         return;
     }
 
+    // filtremlemeye yariyormus
+    const filteredData = Object.fromEntries(
+        Object.entries(restData).filter(([_, value]) => value !== undefined)
+    );
+
     await prisma.ticket.update({
-        where: { 
-            id:ticket.id
-        },
+        where: { id },
+        data: filteredData
+    });
+
+    await prisma.ticket.update({
+        where: { id },
         data: {
-            description,
-            stock,
-            sold,
-            day:newDay,
-            hour:newHour
+            ...restData
         }
     });
-    
-
 
 
     res.status(200).json({
