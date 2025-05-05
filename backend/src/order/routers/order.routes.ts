b435cfd326b27cf855a5cf359f533c284e1d3021
Buraktalhaa <talha45879@gmail.com>
2025-05-05 13:37:42 +0300

    order Routes updated

--- backend/src/order/routers/order.routes.ts
+++ backend/src/order/routers/order.routes.ts
@@ -2,15 +2,15 @@ import { Router } from "express";
 import { authenticateToken } from "../../common/middleware/tokenMiddleware";
 import { permissionControl } from "../../profile/middleware/permissionMiddleware";
 import { createOrder } from "../controller/createOrder";
-import { deleteOrder } from "../controller/deleteOrder";
+import { cancelOrder } from "../controller/cancelOrder";
 import { getOrders } from "../controller/getOrders";
 import { getUserPoints } from "../controller/getPoints"
 
 const router = Router()
 
 router.post('/create-order', authenticateToken, permissionControl, createOrder)
-router.post('/delete-order', authenticateToken, permissionControl, deleteOrder)
-router.get('/orders', authenticateToken, permissionControl, getOrders)
+router.post('/cancel-order', authenticateToken, permissionControl, cancelOrder)
+router.get('/get-orders', authenticateToken, permissionControl, getOrders)
 router.get('/get-points', authenticateToken, permissionControl, getUserPoints)
 
 
