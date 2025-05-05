a9873477e65239226d297c564f396d2b8df965bb
Buraktalhaa <talha45879@gmail.com>
2025-05-05 13:36:37 +0300

    ticket routes updated

--- backend/src/ticket/routers/ticket.routes.ts
+++ backend/src/ticket/routers/ticket.routes.ts
@@ -5,13 +5,16 @@ import { createTicket } from "../controller/createTicket";
 import { deleteTicket } from "../controller/deleteTicket";
 import { editTicket } from "../controller/editTicket";
 import { getTickets } from "../controller/getTickets"
+import { updateTicketStatus } from "../controller/updateTicketStatus";
+import { companyMiddleware } from "../middleware/companyMiddleware";
 
 const router = Router()
 
 
 router.post('/create-ticket', authenticateToken, permissionControl, createTicket)
-router.post('/delete-ticket', authenticateToken, permissionControl, deleteTicket)
-router.post('/edit-ticket', authenticateToken, permissionControl, editTicket)
+router.post('/delete-ticket', authenticateToken, permissionControl, companyMiddleware, deleteTicket)
+router.post('/edit-ticket', authenticateToken, permissionControl, companyMiddleware, editTicket)
+router.post('/update-status', authenticateToken, permissionControl, updateTicketStatus)
 router.get('/get-tickets', getTickets)
 
 export default router;
