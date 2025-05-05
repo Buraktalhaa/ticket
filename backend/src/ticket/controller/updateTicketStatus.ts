27b49844ddb71967b394b122797a9e2dc168eaea
Buraktalhaa <talha45879@gmail.com>
2025-05-05 13:37:29 +0300

    updateTicketStatus created for admin

---
+++ backend/src/ticket/controller/updateTicketStatus.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import prisma from "../../common/utils/prisma";
+import { handleError } from "../../common/error-handling/handleError";
+import { ResponseStatus } from "../../common/enums/status.enum";
+import { DecodedUser } from '../../common/type/request.type';
+
+
+export const updateTicketStatus = async (req: Request, res: Response) => {
+    const { userId } = req.user as DecodedUser;
+    const { ticketId, status } = req.body
+
+    const ticket = await prisma.ticket.findUnique({
+        where: {
+            id: ticketId
+        }
+    });
+
+    if (!ticket) {
+        handleError(res,'ticket not found', 400)
+        return;
+    }
+
+    const updatedTicket = await prisma.ticket.update({
+        where: { id: ticket.id },
+        data: { status }
+    });
+
+    res.status(200).json({
+        status: ResponseStatus.SUCCESS,
+        message: 'ticket status updated succesfully',
+    });
+    return;
+}
