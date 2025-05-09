d455e88f6fcb55234b7ee9d9d86ac470519b2325
Buraktalhaa <talha45879@gmail.com>
2025-05-09 10:32:46 +0300

    redis added for orders

--- backend/src/order/controller/getOrders.ts
+++ backend/src/order/controller/getOrders.ts
@@ -2,21 +2,37 @@ import { Request, Response } from 'express';
 import { DecodedUser } from '../../common/type/request.type';
 import prisma from '../../common/utils/prisma';
 import { ResponseStatus } from '../../common/enums/status.enum';
+import redis from '../../common/utils/redis';
 
 
 export async function getOrders(req:Request, res:Response){
     const { userId } = req.user as DecodedUser;
 
+    const cacheKey = `user:order:${userId}`
 
-    await prisma.order.findMany({
+    const cachedOrder = await redis.get(cacheKey)
+
+    if(cachedOrder){
+        res.status(200).json({
+            status: ResponseStatus.SUCCESS,
+            message: "Your orders with cache",
+            data: JSON.parse(cachedOrder)
+        })
+        return;
+    }
+
+    const orders = await prisma.order.findMany({
         where:{
             userId
         }
     })
 
+    await redis.set(cacheKey, JSON.stringify(orders), 'EX', 50)
+
     res.status(200).json({
         status: ResponseStatus.SUCCESS,
         message: 'User all orders',
+        data:orders
     });
     return;
 }
