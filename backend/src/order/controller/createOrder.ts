39f134da5ffee19c96704473ea4b5578ac1f261a
Buraktalhaa <talha45879@gmail.com>
2025-05-10 15:53:01 +0300

    createOrder changed with new functionality

--- backend/src/order/controller/createOrder.ts
+++ backend/src/order/controller/createOrder.ts
@@ -3,7 +3,7 @@ import { DecodedUser } from '../../common/type/request.type';
 import { handleError } from '../../common/error-handling/handleError';
 import { ResponseStatus } from '../../common/enums/status.enum';
 import { orderQueue } from '../queue/orderQueue';
-import '../queue/worker';
+import '../queue/orderWorker';
 import redis from '../../common/utils/redis';
 import { wait } from '../../common/utils/wait';
 
@@ -22,11 +22,25 @@ export async function createOrder(req: Request, res: Response) {
         ticketId,
         quantity,
         usePoints
+    }, {
+        attempts: 3,      // try 3 times
+        backoff: {
+            type: 'fixed',
+            delay: 3000   //  3 seconds
+        }
     });
 
-    await wait(3000);
+    await wait(3000)
 
-    const paymentLink = await redis.get(`payment-link:${userId}:${job.id}`);
+    let paymentLink: string | null = null;
+    const redisKey = `payment-link:${userId}:${job.id}`;
+    for (let i = 0; i < 60; i++) {
+        paymentLink = await redis.get(redisKey);
+        if (paymentLink) {
+            break;
+        }
+        await wait(1000);
+    }
 
     if (paymentLink) {
         res.status(200).json({
@@ -36,7 +50,6 @@ export async function createOrder(req: Request, res: Response) {
             paymentLink,
         });
         return
-
     } else {
         handleError(res, "Payment link can't created", 404)
         return
