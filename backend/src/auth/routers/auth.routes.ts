b9ecacd5932fb9b42d693c8b18044e8adf2c925c
Buraktalhaa <talha45879@gmail.com>
2025-05-11 11:04:01 +0300

    routes updated

--- backend/src/auth/routers/auth.routes.ts
+++ backend/src/auth/routers/auth.routes.ts
@@ -4,14 +4,21 @@ import { signInController } from '../controllers/sign-in';
 import { refreshController } from '../controllers/refresh';
 import { forgotPasswordController } from '../controllers/forgot-password';
 import { resetPasswordController } from '../controllers/resetPassword';
+import passport from 'passport';
+import { googleCallback } from '../controllers/googleCallback';
+import '../auth/googleStrategy';
 
 
 const router = Router();
 
 router.post('/signIn', signInController);
 router.post('/signUp', signUpController);
+
 router.post('/refresh', refreshController)
 router.post('/forgot-password', forgotPasswordController)
 router.post('/reset-password/:token', resetPasswordController)
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { session: false }), googleCallback)
 
 export default router;
