713668e910b2e53c0bbccf6a8b4158d33bc41b79
Buraktalhaa <talha45879@gmail.com>
2025-05-12 16:58:20 +0300

    jobEvents created

---
+++ backend/src/order/queue/jobEvents.ts
@@ -0,0 +1,11 @@
+import { Job } from "bullmq";
+import { orderQueue } from "./orderQueue";
+
+export const handleFailedJob = async (job: Job, error: Error) => {
+    if (!job) {
+        console.error(`Job with ID ${job} not found.`);
+        return;
+    }
+    console.error(`❌ Job ${job.id} failed: ${error.message}`);
+    await orderQueue.add('create-order', job.data);
+};
