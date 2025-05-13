5a5c22cf0fe1fd5d6cbfecbbc35a73cabe8100b1
Buraktalhaa <talha45879@gmail.com>
2025-05-13 17:27:04 +0300

    auth-input component created

---
+++ frontend/src/app/auth/shared/components/auth-input/auth-input.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-input',
+  imports: [CommonModule],
+  templateUrl: './auth-input.component.html',
+  styleUrl: './auth-input.component.css',
+})
+export class AuthInputComponent {
+  public isFocused: boolean = false;
+
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() src: string = '';
+  @Input() type: string = 'text';
+  @Input() model: string = '';
+  @Output() modelChange = new EventEmitter<string>();
+
+  constructor() {}
+
+  onFocus() {
+    this.isFocused = true;
+  }
+  onBlur() {
+    this.isFocused = false;
+  }
+
+  onChange(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    console.log("Deneme yeri", value)
+    this.model = value;
+    this.modelChange.emit(value);
+  }
+}
