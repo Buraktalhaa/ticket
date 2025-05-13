33b78746a7574ed0ccb3315d743bcbbfdd210384
Buraktalhaa <talha45879@gmail.com>
2025-05-13 17:26:14 +0300

    main routes created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', component: MainComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'signin', component: SigninComponent },
+    { path: '**', redirectTo: '/main' }
+];
