b70d20a24ad4b5896f8e641ef99f9763061286cd
Buraktalhaa <talha45879@gmail.com>
2025-05-14 13:25:04 +0300

    Update Sign In page with new UI design

--- frontend/src/app/auth/signin/signin.component.ts
+++ frontend/src/app/auth/signin/signin.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthInputComponent } from '../shared/components/auth-input/auth-input.component';
 import { AuthService } from '../shared/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-signin',
-  imports: [AuthInputComponent],
+  imports: [
+    AuthInputComponent,
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
@@ -18,12 +26,13 @@ export class SigninComponent {
       console.log("Missing information");
       return;
     }
+    console.log("Degerler", this.email, this.password)
 
     const signInData = {
       email: this.email,
       password: this.password,
     };
-    
+
     this.authService.signIn(signInData)
   }
 }
