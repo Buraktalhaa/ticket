797e24995ced5dd70939c74d0aacfd248897e5dc
Buraktalhaa <talha45879@gmail.com>
2025-05-14 13:24:20 +0300

    interceptor path added

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { notificationInterceptor } from './shared/interceptors/notification.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideAnimationsAsync(),provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideAnimationsAsync(),provideHttpClient(withInterceptors([notificationInterceptor]))]
 };
