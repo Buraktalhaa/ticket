df102417b4c0f24901000d7cfb89748ed421af7b
Buraktalhaa <talha45879@gmail.com>
2025-05-14 16:48:59 +0300

    forgot-password updated

--- backend/src/auth/controllers/forgot-password.ts
+++ backend/src/auth/controllers/forgot-password.ts
@@ -7,7 +7,7 @@ import { createToken } from "../utils/createToken";
 export async function forgotPasswordController(req: Request, res: Response) {
     try {
         const { email } = req.body;
-        
+
         const user = await prisma.user.findUnique({
             where: {
                 email
@@ -19,16 +19,16 @@ export async function forgotPasswordController(req: Request, res: Response) {
             return
         }
 
-        const token = createToken(user.id, email, process.env.ACCESS_SECRET!, 10 * 2)
-
         const oldToken = await prisma.passwordResetToken.findUnique({
             where: {
                 userId: user.id
             }
         })
 
-        // if token exists 
+        const token = createToken(user.id, email, process.env.ACCESS_SECRET!, 10 * 60 * 1000)
+
         if (oldToken && oldToken.expiresAt < new Date()) {
+            
             await prisma.passwordResetToken.update({
                 where: {
                     userId: user.id
@@ -40,26 +40,26 @@ export async function forgotPasswordController(req: Request, res: Response) {
                 }
             })
         }
-        else if(oldToken && oldToken.expiresAt >= new Date()){
+
+        else if (oldToken && oldToken.expiresAt >= new Date()) {
             handleError(res, 'Clicked in 10 minutes', 400)
             return;
+
+        } else {
+            await prisma.passwordResetToken.create({
+                data: {
+                    token,
+                    userId: user.id,
+                    expiresAt: new Date(Date.now() + 10 * 60 * 1000)
+                }
+            })
         }
 
-        // Save token in db
-        await prisma.passwordResetToken.create({
-            data: {
-                token,
-                userId: user.id,
-                expiresAt: new Date(Date.now() + 10 * 60 * 1000)
-            }
-        })
-
-        const mail = new Email({ email, firstName: user.firstName }, `http://localhost:3000/auth/reset-password/${token}`);
+        const mail = new Email({ email, firstName: user.firstName }, `http://localhost:4200/reset-password/${token}`);
         await mail.send('Subject', 'forgot Password?');
 
         res.status(200).json({
             message: "Welcome email sent successfully",
-            resetToken: token // TODO:deleted after this process done
         });
         return;
     }
