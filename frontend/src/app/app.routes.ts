4ea1c0824a631c92b0f08a8f5503ba231e7f7288
Buraktalhaa <talha45879@gmail.com>
2025-05-14 13:21:53 +0300

    main routes updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { MainComponent } from './main/main.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { MailSentComponent } from './auth/mail-sent/mail-sent.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'signin', component: SigninComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'mail-sent', component: MailSentComponent },
     { path: '**', redirectTo: '/main' }
 ];
