d92672fb33575616e629d9c858b41f676c1effc5
Buraktalhaa <talha45879@gmail.com>
2025-05-15 09:40:47 +0300

    app.routes updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { MainComponent } from './main/main.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { MailSentComponent } from './auth/mail-sent/mail-sent.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { MainComponent } from './main/mainPage/main.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
@@ -12,5 +13,6 @@ export const routes: Routes = [
     { path: 'signin', component: SigninComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'mail-sent', component: MailSentComponent },
+    { path: 'reset-password/:token', component: ResetPasswordComponent },
     { path: '**', redirectTo: '/main' }
 ];
