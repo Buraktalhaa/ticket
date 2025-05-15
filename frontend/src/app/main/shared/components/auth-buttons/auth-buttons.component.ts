05eeb79f53c1e32762c296fe4cbf476b108aa292
Buraktalhaa <talha45879@gmail.com>
2025-05-15 08:37:37 +0300

    feat: add auth-buttons component

---
+++ frontend/src/app/main/shared/components/auth-buttons/auth-buttons.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-auth-buttons',
+  imports: [RouterLink],
+  templateUrl: './auth-buttons.component.html',
+  styleUrl: './auth-buttons.component.css'
+})
+export class AuthButtonsComponent {
+
+}
