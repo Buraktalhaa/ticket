a38bfd6ad8b9fd113ad2dfa1aff583dd7f799723
Buraktalhaa <talha45879@gmail.com>
2025-05-16 09:10:18 +0300

    feat: token check guard created

---
+++ frontend/src/app/auth/shared/guards/can-activete.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const canActiveteGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const isUserLoggedIn = authService.isThereUser();
+
+  if (!isUserLoggedIn) {
+    router.navigateByUrl('/signin');
+    return false;
+  }
+
+  return true;
+};
