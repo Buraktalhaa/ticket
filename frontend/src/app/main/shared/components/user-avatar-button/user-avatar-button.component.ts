4665adce90f32fe7e45a5018ea09f6c195cab12f
Buraktalhaa <talha45879@gmail.com>
2025-05-16 09:14:24 +0300

    feat: user-avatar button created

---
+++ frontend/src/app/main/shared/components/user-avatar-button/user-avatar-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-avatar-button',
+  imports: [],
+  templateUrl: './user-avatar-button.component.html',
+  styleUrl: './user-avatar-button.component.css'
+})
+export class UserAvatarButtonComponent {
+
+}
