0be4ade2e39ed89ff15366b0e22c5e3549ff9ff9
Buraktalhaa <talha45879@gmail.com>
2025-05-16 09:12:41 +0300

    new routes added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.
 import { MailSentComponent } from './auth/mail-sent/mail-sent.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
 import { MainComponent } from './main/mainPage/main.component';
+import { MyProfileComponent } from './profile/my-profile/my-profile.component';
+import { canActiveteGuard } from './auth/shared/guards/can-activete.guard';
+import { CreateTicketComponent } from './ticket/seller/create-ticket/create-ticket.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
@@ -14,5 +17,7 @@ export const routes: Routes = [
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'mail-sent', component: MailSentComponent },
     { path: 'reset-password/:token', component: ResetPasswordComponent },
-    { path: '**', redirectTo: '/main' }
+    { path: 'my-profile', component: MyProfileComponent, canActivate:[canActiveteGuard]},
+    { path: 'seller/create-ticket', component: CreateTicketComponent },
+    { path: '**', redirectTo: '/main'}
 ];
