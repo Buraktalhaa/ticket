867ac84ce75a2c4689da247a5efdce1a52490cb1
Buraktalhaa <talha45879@gmail.com>
2025-05-16 09:14:00 +0300

    main component updated

--- frontend/src/app/main/mainPage/main.component.ts
+++ frontend/src/app/main/mainPage/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { AuthService } from '../../auth/shared/services/auth.service';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +9,11 @@ import { NavbarComponent } from '../shared/components/navbar/navbar.component';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  constructor(private authService: AuthService){}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(!!localStorage.getItem("accessToken"));
+    this.authService.isThereUser.set(!!localStorage.getItem("accessToken")) 
+  }
 }
