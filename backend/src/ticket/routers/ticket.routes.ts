ac332659a20d56efe5cd95ab5135c463d83100ec
Buraktalhaa <talha45879@gmail.com>
2025-05-18 00:22:53 +0300

    ticketRoutes updated for admin status panel

--- backend/src/ticket/routers/ticket.routes.ts
+++ backend/src/ticket/routers/ticket.routes.ts
@@ -9,15 +9,19 @@ import { updateTicketStatus } from "../controller/changeTicketStatus";
 import { companyMiddleware } from "../middleware/companyMiddleware";
 import { isSeller } from "../controller/isSeller";
 import { sellerTickets } from "../controller/sellerTickets";
+import { statusPanel } from "../controller/statusPanel";
 
 const router = Router()
 
 router.post('/create-ticket', authenticateToken, permissionControl, createTicket)
 router.post('/delete-ticket', authenticateToken, permissionControl, companyMiddleware, deleteTicket)
-router.post('/edit-ticket', authenticateToken, permissionControl, companyMiddleware, editTicket)
-router.post('/update-status', authenticateToken, permissionControl, updateTicketStatus)
 router.get('/get-tickets', getTickets)
+
 router.get('/is-seller', authenticateToken, permissionControl, isSeller)
 router.get('/seller/sellerTickets', authenticateToken, permissionControl, sellerTickets)
+router.post('/edit-ticket', authenticateToken, permissionControl, companyMiddleware, editTicket)    //TODO:
+
+router.get('/admin/statusPanel', authenticateToken, permissionControl, statusPanel)
+router.post('admin/stausPanel/update-status', authenticateToken, permissionControl, updateTicketStatus)
 
 export default router;
