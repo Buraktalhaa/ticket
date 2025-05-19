86013bee2110b3126994241f72fcf71fca1f6eb3
Buraktalhaa <talha45879@gmail.com>
2025-05-19 11:43:51 +0300

    feat: label component created

---
+++ frontend/src/app/ticket/seller/labels/labels.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-labels',
+  imports: [],
+  templateUrl: './labels.component.html',
+  styleUrl: './labels.component.css'
+})
+export class LabelsComponent {
+  @Input() label!: string;
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text'; // input tipi
+  @Input() model: any;
+  @Output() modelChange = new EventEmitter<any>();
+
+  isFocused: boolean = false;
+
+  onChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.modelChange.emit(target.value);
+  }
+
+  onFocus() {
+    this.isFocused = true;
+  }
+
+  onBlur() {
+    this.isFocused = false;
+  }
+}
