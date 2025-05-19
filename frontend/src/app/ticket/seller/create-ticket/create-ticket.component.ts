0b01abe6a313e2618b9af31e13eada32f1ce3555
Buraktalhaa <talha45879@gmail.com>
2025-05-19 11:45:13 +0300

    create-ticket component created

--- frontend/src/app/ticket/seller/create-ticket/create-ticket.component.ts
+++ frontend/src/app/ticket/seller/create-ticket/create-ticket.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.component';
+import { LabelsComponent } from '../labels/labels.component';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { TicketService } from '../../shared/services/ticket.service';
 
 @Component({
   selector: 'app-create-ticket',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    LabelsComponent,
+    FormsModule
+  ],
   templateUrl: './create-ticket.component.html',
   styleUrl: './create-ticket.component.css'
 })
 export class CreateTicketComponent {
+  ticket = {
+    categoryName: '',
+    description: '',
+    hour: 0,
+    day: '',
+    stock: 0,
+    price: 0,
+    pointRate: 0,
+    pointExpiresAt: '',
+    discount: 0,
+  };
 
+  constructor(private http: HttpClient,
+    private ticketServie:TicketService
+  ) {}
+
+  categoryOptions = ['Concert', 'Hotel', 'Event', 'Train', 'Bus'];
+
+  createTicket() {
+    this.ticketServie.createTicket(this.ticket)
+  }
 }
