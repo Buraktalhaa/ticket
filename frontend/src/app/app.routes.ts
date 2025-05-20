73b42860add6633227ba4030b189b1c4934f2b55
Buraktalhaa <talha45879@gmail.com>
2025-05-20 09:50:36 +0300

    role based guard added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
-import { MailSentComponent } from './auth/mail-sent/mail-sent.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { MainComponent } from './main/mainPage/main.component';
+import { ForgotPasswordComponent } from './auth/components/forgot-password/forgot-password.component';
+import { MailSentComponent } from './auth/components/mail-sent/mail-sent.component';
 import { MyProfileComponent } from './profile/my-profile/my-profile.component';
 import { CreateTicketComponent } from './ticket/seller/create-ticket/create-ticket.component';
 import { TicketsComponent } from './ticket/user/tickets/tickets.component';
@@ -14,6 +10,10 @@ import { SellerTicketsComponent } from './ticket/seller/seller-tickets/seller-ti
 import { TicketEditComponent } from './ticket/seller/ticket-edit/ticket-edit.component';
 import { StatusPanelComponent } from './ticket/admin/status-panel/status-panel.component';
 import { StatusPanelEditComponent } from './ticket/admin/status-panel-edit/status-panel-edit.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { SigninComponent } from './auth/components/signin/signin.component';
+import { ResetPasswordComponent } from './auth/components/reset-password/reset-password.component';
+import { MainComponent } from './main/components/mainPage/main.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
@@ -23,14 +23,14 @@ export const routes: Routes = [
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'mail-sent', component: MailSentComponent },
     { path: 'reset-password/:token', component: ResetPasswordComponent },
-    { path: 'my-profile', component: MyProfileComponent, canActivate:[canActiveteGuard]},
-    { path: 'create-ticket', component: CreateTicketComponent },
+    { path: 'my-profile', component: MyProfileComponent, canActivate:[canActiveteGuard],data:{roles:['user']}},
+    { path: 'create-ticket', component: CreateTicketComponent, canActivate:[canActiveteGuard],data:{roles:['seller']} },
     { path: 'tickets', component: TicketsComponent },
     { path: 'unauthorized', component: UnauthorizedComponent },
-    { path: 'seller/sellerTickets', component:  SellerTicketsComponent},
-    { path: 'seller/tickets/edit', component: TicketEditComponent}, // TODO:
-    { path: 'admin/statusPanel/tickets', component: StatusPanelComponent},
-    { path: 'admin/statusPanel/tickets/edit', component: StatusPanelEditComponent},
+    { path: 'seller/tickets', component:  SellerTicketsComponent, canActivate:[canActiveteGuard],data:{roles:['seller']}},
+    { path: 'seller/tickets/edit', component: TicketEditComponent, canActivate:[canActiveteGuard],data:{roles:['seller']}},
+    { path: 'admin/statusPanel/tickets', component: StatusPanelComponent, canActivate:[canActiveteGuard],data:{roles:['admin']}},
+    { path: 'admin/statusPanel/tickets/edit', component: StatusPanelEditComponent, canActivate:[canActiveteGuard],data:{roles:['admin']}},
 
     { path: '**', redirectTo: '/main'}
 ];
--- frontend/src/app/guards/can-activete.guard.ts
+++ frontend/src/app/guards/can-activete.guard.ts
@@ -1,8 +1,8 @@
-import { CanActivateFn, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../auth/shared/services/auth.service';
 
-export const canActiveteGuard: CanActivateFn = () => {
+export const canActiveteGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const router = inject(Router);
   const authService = inject(AuthService);
 
@@ -10,6 +10,15 @@ export const canActiveteGuard: CanActivateFn = () => {
 
   if (!isUserLoggedIn) {
     router.navigateByUrl('/signin');
+    return false;
+  }
+
+  const expectedRoles: string[] = route.data['roles'];
+  const role = localStorage.getItem("role") || '';
+
+
+  if (expectedRoles && !expectedRoles.includes(role)) {
+    router.navigateByUrl('/main');    
     return false;
   }
 
