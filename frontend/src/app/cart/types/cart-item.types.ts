f21d8b60631e6aad3022f18d55a051dbf04a6e1b
Buraktalhaa <talha45879@gmail.com>
2025-05-21 17:15:42 +0300

    feat: cart-item-types created

---
+++ frontend/src/app/cart/types/cart-item.types.ts
@@ -0,0 +1,23 @@
+export interface Ticket {
+    id: string;
+    title: string;
+    description?: string;
+    price: number;
+    stock?: number;
+    discount?: number;
+    images?: string[];
+    day?: string | Date;
+    hour?: number;
+    company?: {
+      name: string;
+    };
+    category?: {
+      name: string;
+    };
+  }
+  
+  export interface CartItem {
+    ticket: Ticket;
+    quantity: number;
+  }
+  
