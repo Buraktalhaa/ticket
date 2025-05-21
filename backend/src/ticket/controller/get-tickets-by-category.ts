5ca6a4479f2949e2ba07a3dbef03de5df58bd43a
Buraktalhaa <talha45879@gmail.com>
2025-05-21 09:02:17 +0300

    get-tickets-by-category file was created to fetch tickets according to category.

---
+++ backend/src/ticket/controller/get-tickets-by-category.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import prisma from "../../common/utils/prisma";
+import { ResponseStatus } from "../../common/enums/status.enum";
+
+export async function getTicketsByCategory(req: Request, res: Response) {
+
+    const categoryName = req.params.category;
+    console.log(categoryName);
+
+    try {
+        const tickets = await prisma.ticket.findMany({
+            where: {
+                sold: false,
+                status: "approve",
+                category: {
+                    name: categoryName, 
+                },
+            },
+            include: {
+                category: true,
+                company: true,
+                user: true,
+            },
+        });
+
+        res.status(200).json({
+            status: ResponseStatus.SUCCESS,
+            message: `Tickets for category: ${categoryName}`,
+            data: tickets,
+        });
+    } catch (error) {
+        console.error("Error fetching tickets by category:", error);
+        res.status(500).json({
+            status: ResponseStatus.FAIL,
+            message: "An error occurred while fetching tickets.",
+        });
+    }
+
+}
