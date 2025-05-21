c7504ad5da77036383af2e2e7c4763ac486e7dcb
Buraktalhaa <talha45879@gmail.com>
2025-05-21 17:16:27 +0300

    cart component and cart service implemented

--- frontend/src/app/cart/components/cart/cart.component.ts
+++ frontend/src/app/cart/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartItem } from '../../types/cart-item.types';
 import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.component';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,5 +12,37 @@ import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.c
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  item: CartItem | null = null;
 
+  constructor(
+    private cartService: CartService
+  ) {}
+
+  // sayfa ilk yuklenince direkt yuklmesi lazim
+  ngOnInit() {
+    this.loadCart();
+  }
+
+  loadCart() {
+    this.item = this.cartService.getCurrentItem();
+  }
+
+  increaseQuantity() {
+    if (!this.item) {
+      return;
+    }
+    this.item.quantity++;
+    this.cartService.updateItem(this.item);
+  }
+
+  decreaseQuantity() {
+    if (!this.item || this.item.quantity <= 1) return;
+    this.item.quantity--;
+    this.cartService.updateItem(this.item);
+  }
+
+  removeItem() {
+    this.cartService.clearCart();
+    this.item = null;
+  }
 }
--- frontend/src/app/cart/services/cart.service.ts
+++ frontend/src/app/cart/services/cart.service.ts
@@ -1,10 +1,54 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../types/cart-item.types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private cartItem: CartItem | null = null;
 
-  constructor() { }
-  
+  constructor() {
+    this.loadFromStorage();
+  }
+
+  private saveToStorage() {
+    if (this.cartItem) {
+      localStorage.setItem('cartItem', JSON.stringify(this.cartItem));
+    } else {
+      localStorage.removeItem('cartItem');
+    }
+  }
+
+  private loadFromStorage() {
+    const item = localStorage.getItem('cartItem');
+    if (item) {
+      this.cartItem = JSON.parse(item);
+    }
+  }
+
+  getCurrentItem(): CartItem | null {
+    return this.cartItem;
+  }
+
+  addToCart(item: CartItem) {
+    console.log(item)
+    this.cartItem = item ;
+    console.log(this.cartItem)
+    this.saveToStorage();
+  }
+
+  updateItem(item: CartItem) {
+    if (!this.cartItem || this.cartItem.ticket.id !== item.ticket.id) {
+      throw new Error('Güncellenecek ürün sepette yok ya da farklı ürün');
+    }
+    this.cartItem = { ...item };
+    this.saveToStorage();
+  }
+
+  clearCart() {
+    console.log("1",this.cartItem)
+    this.cartItem = null;
+    console.log("2",this.cartItem)
+    this.saveToStorage();
+  }
 }
