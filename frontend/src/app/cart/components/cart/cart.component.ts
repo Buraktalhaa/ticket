48f7e475925f827dc46b8a05d321a0bf179d1b19
Buraktalhaa <talha45879@gmail.com>
2025-05-22 15:20:45 +0300

    cart component updated

--- frontend/src/app/cart/components/cart/cart.component.ts
+++ frontend/src/app/cart/components/cart/cart.component.ts
@@ -2,11 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../types/cart-item.types';
 import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.component';
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    CommonModule
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
@@ -15,34 +18,69 @@ export class CartComponent {
   item: CartItem | null = null;
 
   constructor(
-    private cartService: CartService
-  ) {}
+    private cartService: CartService,
+    private router: Router
+  ) { }
 
-  // sayfa ilk yuklenince direkt yuklmesi lazim
   ngOnInit() {
     this.loadCart();
   }
 
   loadCart() {
-    this.item = this.cartService.getCurrentItem();
+    this.cartService.getCurrentItem().subscribe(cartItem => {
+      this.item = cartItem;
+    });
   }
 
   increaseQuantity() {
-    if (!this.item) {
-      return;
-    }
-    this.item.quantity++;
-    this.cartService.updateItem(this.item);
+    if (!this.item) return;
+    const updatedCount = this.item.count + 1;
+    this.cartService.updateItem({
+      ticketId: this.item.ticket.id,
+      count: updatedCount
+    }).subscribe(() => {
+      this.item!.count = updatedCount;
+    });
   }
-
+  
   decreaseQuantity() {
-    if (!this.item || this.item.quantity <= 1) return;
-    this.item.quantity--;
-    this.cartService.updateItem(this.item);
+    if (!this.item || this.item.count <= 1) return;
+    const updatedCount = this.item.count - 1;
+    this.cartService.updateItem({
+      ticketId: this.item.ticket.id,
+      count: updatedCount
+    }).subscribe(() => {
+      this.item!.count = updatedCount;
+    });
   }
 
   removeItem() {
-    this.cartService.clearCart();
-    this.item = null;
+    this.cartService.clearCart().subscribe(() => {
+      this.item = null;
+    });
+  }
+
+  goToTicketDetail(ticketId: string, category: string){
+    this.router.navigate(['/main', category, ticketId]);
+  }
+
+  buyTicket() {
+    const data = {
+      ticketId: this.item?.ticket.id,
+      quantity: this.item?.count,
+      usePoints: true
+    };
+  
+    this.cartService.buy(data).subscribe(response => {
+      const paymentLink = response.body.paymentLink;
+      if (paymentLink) {
+        // redirect
+        window.location.href = paymentLink;
+      } else {
+        alert('Ödeme linki oluşturulamadı, lütfen tekrar deneyin.');
+      }
+    }, error => {
+      alert('Sipariş oluşturulurken bir hata oluştu.');
+    });
   }
 }
