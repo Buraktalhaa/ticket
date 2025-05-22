30a13b07849891b8c9d61e4e9a02714be713a091
Buraktalhaa <talha45879@gmail.com>
2025-05-22 15:18:01 +0300

    feat(frontend): order-service created and impelemented

---
+++ frontend/src/app/order/services/order.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(
+    private apiService:ApiService
+  ) { }
+
+  getMyOrders(): Observable<any> {
+    return this.apiService.get(`http://localhost:3000/order/get-orders`);
+  }
+}
