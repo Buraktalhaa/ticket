08e43942e815060441d267dc065ee66ec83fe9dc
Buraktalhaa <talha45879@gmail.com>
2025-05-22 15:19:05 +0300

    get-orders changed

--- backend/src/order/controller/get-orders.ts
+++ backend/src/order/controller/get-orders.ts
@@ -2,37 +2,36 @@ import { Request, Response } from 'express';
 import { DecodedUser } from '../../common/type/request.type';
 import prisma from '../../common/utils/prisma';
 import { ResponseStatus } from '../../common/enums/status.enum';
-import redis from '../../common/utils/redis';
 
-
-export async function getOrders(req:Request, res:Response){
+export async function getOrders(req: Request, res: Response) {
     const { userId } = req.user as DecodedUser;
 
-    const cacheKey = `user:order:${userId}`
-
-    const cachedOrder = await redis.get(cacheKey)
-
-    if(cachedOrder){
-        res.status(200).json({
-            status: ResponseStatus.SUCCESS,
-            message: "Your orders with cache",
-            data: JSON.parse(cachedOrder)
-        })
-        return;
-    }
-
     const orders = await prisma.order.findMany({
-        where:{
-            userId
-        }
-    })
+        where: {
+            userId,
+        },
+        select: {
+            id: true,
+            createdAt: true,
+            orderDay: true,
+            orderHour: true,
+            quantity: true,
+            totalAmount: true,
+            status: true,
+            ticket: {
+                select: {
+                    title: true,
+                    description: true,
+                },
+            },
+        },
+    });
 
-    await redis.set(cacheKey, JSON.stringify(orders), 'EX', 50)
 
     res.status(200).json({
         status: ResponseStatus.SUCCESS,
         message: 'User all orders',
-        data:orders
+        data: orders
     });
     return;
 }
