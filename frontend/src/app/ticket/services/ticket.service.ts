2007b25f9324aebd31921bdbbcd2b3be13094304
Buraktalhaa <talha45879@gmail.com>
2025-05-23 11:17:15 +0300

    Split ticket service into seller, admin, token services

--- frontend/src/app/ticket/services/ticket.service.ts
+++ frontend/src/app/ticket/services/ticket.service.ts
@@ -3,7 +3,7 @@ import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { ApiService } from '../../shared/services/api.service';
-import { CreateTicketDTO, Ticket } from '../types/ticket.types';
+import { Ticket } from '../types/ticket.types';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +13,6 @@ export class TicketService {
 
   private ticketsSubject = new BehaviorSubject<Ticket[]>([]);
   tickets$ = this.ticketsSubject.asObservable();
-
-  private sellerTickets = new BehaviorSubject<Ticket[]>([]);
-  sellerTickets$ = this.sellerTickets.asObservable();
-
-  private adminTickets = new BehaviorSubject<Ticket[]>([]);
-  adminTickets$ = this.adminTickets.asObservable();
 
   constructor(
     private api: ApiService,
@@ -50,81 +44,6 @@ export class TicketService {
         console.error('ticket category error:', err);
       }
     });
-  }
-
-  getMyTickets() {
-    this.api.get('http://localhost:3000/ticket/seller/seller-tickets')
-      .subscribe({
-        next: (res: HttpResponse<any>) => {
-          const data = res.body?.data;
-          this.sellerTickets.next(data);
-          this.router.navigateByUrl('seller-dashboard/my-tickets')
-        },
-        error: (err) => {
-          console.error('ticket error:', err);
-        }
-      });
-  }
-
-  routeToCreateTicketPage() {
-    this.api.get('http://localhost:3000/ticket/is-seller').subscribe({    //Seller dashbooard yapacaksin hem back hem front
-      next: (res: HttpResponse<any>) => {
-        const data = res.body?.data;
-        this.router.navigateByUrl('seller-dashboard/create-ticket')
-      },
-      error: (err) => {
-        console.error('ticket error:', err);
-        // this.router.navigateByUrl('/unauthorized');
-      }
-    });
-  }
-
-  createTicket(ticket: CreateTicketDTO) {
-    this.api.post('http://localhost:3000/ticket/create-ticket', ticket)
-      .subscribe({
-        next: () => {
-          alert('Ticket created successfully')
-          this.router.navigateByUrl('seller-dashboard/my-tickets')
-        },
-        error: (err) => alert('Error: ' + err.error?.message || 'Something went wrong')
-      });
-  }
-
-  editTicket(ticket: Ticket | null) {
-    this.api.post('http://localhost:3000/ticket/edit-ticket', ticket)
-      .subscribe({
-        next: (res: HttpResponse<any>) => {
-          alert('Ticket edited successfully')
-          this.router.navigateByUrl('seller-dashboard/my-tickets')
-        },
-        error: (err) => alert('Error: ' + err.error?.message || 'Something went wrong')
-      });
-  }
-
-  goAdminStatusPanel() {
-    this.api.get('http://localhost:3000/ticket/admin/status-panel')
-      .subscribe({
-        next: (res: HttpResponse<any>) => {
-          const data = res.body?.data;
-          this.adminTickets.next(data);
-          this.router.navigateByUrl('admin-dashboard/status-panel')
-        },
-        error: (err) => {
-          console.error('ticket error:', err);
-          // this.router.navigateByUrl('/unauthorized');
-        }
-      })
-  }
-
-  editStatus(ticket: { id:string, status:string }) {
-    this.api.post('http://localhost:3000/ticket/admin/status-panel/update-status', ticket)
-      .subscribe({
-        next: () => {
-          alert('Ticket edited successfully')
-          this.router.navigateByUrl('admin-dashboard/status-panel')
-        },
-        error: (err) => alert('Error: ' + err.error?.message || 'Something went wrong')
-      });
   }
 
   setSelectedTicket(ticket: Ticket) {
