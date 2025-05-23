bcf850dc9cc8fc05389882cf133713e3ded6d7fb
Buraktalhaa <talha45879@gmail.com>
2025-05-23 11:13:18 +0300

    token service created

---
+++ frontend/src/app/auth/services/token.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { DecodedToken } from '../types/auth.type';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  constructor(private cookie: CookieService) {}
+
+  setTokens(accessToken: string, refreshToken: string, role: string) {
+    this.cookie.set('accessToken', accessToken, undefined, '/');
+    this.cookie.set('refreshToken', refreshToken, undefined, '/');
+    this.cookie.set('role', role, undefined, '/');
+  }
+
+  deleteTokens() {
+    this.cookie.delete('accessToken', '/');
+    this.cookie.delete('refreshToken', '/');
+    this.cookie.delete('role', '/');
+  }
+
+  decodeJwt(token: string): DecodedToken {
+    return jwtDecode(token);
+  }
+
+  hasValidTokens(): boolean {
+    return this.cookie.check('accessToken') || this.cookie.check('refreshToken');
+  }
+}
