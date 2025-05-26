51e43c367b7be016c4fab2b6b1d4e532e9b8686e
Buraktalhaa <talha45879@gmail.com>
2025-05-26 13:50:22 +0300

    edit button added

--- frontend/src/app/profile/components/my-profile/my-profile.component.ts
+++ frontend/src/app/profile/components/my-profile/my-profile.component.ts
@@ -3,20 +3,27 @@ import { Profile } from '../../types/profile.type';
 import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.component';
 import { ProfileService } from '../../services/profile.service';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
   imports: [
     NavbarComponent,
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css'
 })
 export class MyProfileComponent {
   user!: Profile;
+  editMode = false;
+  profileForm!: FormGroup;
 
-  constructor(private profileService: ProfileService){}
+  constructor(
+    private profileService: ProfileService,
+    private fb: FormBuilder
+  ){}
 
   ngOnInit(): void {
     this.profileService.profileData$.subscribe(data => {
@@ -24,6 +31,39 @@ export class MyProfileComponent {
         this.user = data;
       } else {
         this.profileService.myProfile();
+      }
+    });
+  }
+  initForm() {
+    this.profileForm = this.fb.group({
+      firstName: [this.user.firstName],
+      lastName: [this.user.lastName],
+      email: [this.user.email],
+      birthday: [this.user.birthday ? new Date(this.user.birthday) : null],
+    });
+  }
+
+  toggleEdit() {
+    this.editMode = !this.editMode;
+    if (this.editMode) {
+      this.initForm();
+    }
+  }
+
+  saveProfile() {
+    if (!this.profileForm.valid) return;
+  
+    const updatedData = this.profileForm.value;
+    this.profileService.updateProfile(updatedData).subscribe({
+      next: (response) => {
+        const updatedUser = response.body;
+        if (updatedUser) {
+          this.user = updatedUser;
+          this.editMode = false;
+        }
+      },
+      error: () => {
+        alert('Profile update failed.');
       }
     });
   }
