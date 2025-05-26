0d30ef2f7cf059e39ec8b2d8067b6feb64e96f6a
Buraktalhaa <talha45879@gmail.com>
2025-05-26 13:50:09 +0300

    auth guard created

---
+++ frontend/src/app/shared/guard/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const isUserLoggedIn = authService.isThereUser();
+
+  if (!isUserLoggedIn) {
+    router.navigate(['/sign-in'], {
+      queryParams: { returnUrl: state.url }
+    });
+    return false;
+  }
+
+  return true;
+};
