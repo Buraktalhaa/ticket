a843bacdf02560e6191f26520c865d3c131da130
Buraktalhaa <talha45879@gmail.com>
2025-05-27 14:46:07 +0300

    unnecessary spaces deleted

--- frontend/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../main/shared/components/navbar/navbar.component';
-import { TicketService } from '../../../ticket/services/ticket.service';
 import { FooterInfoTextComponent } from '../../../shared/components/footer-info-text/footer-info-text.component';
 import { AdminService } from '../../services/admin.service';
 import { AdminNavigationService } from '../../services/admin-navigation.service';
