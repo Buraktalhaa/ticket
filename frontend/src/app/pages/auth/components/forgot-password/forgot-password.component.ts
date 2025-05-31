ef84ead6f290d8a9e7f8f408268a0c1901ce8870
Buraktalhaa <talha45879@gmail.com>
2025-05-31 14:36:56 +0300

    forgot password function changed

--- frontend/src/app/pages/auth/components/forgot-password/forgot-password.component.ts
+++ frontend/src/app/pages/auth/components/forgot-password/forgot-password.component.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { SignButtonComponent } from '../../shared/components/sign-button/sign-button.component';
 import { PasswordService } from '../../services/password.service';
 import { FooterInfoTextComponent } from '../../../../shared/components/footer-info-text/footer-info-text.component';
+import { NotificationService } from '../../../../shared/services/notification.service';
+import { Router } from '@angular/router';
+import { ResetMailService } from '../../services/reset-mail.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -22,13 +25,25 @@ export class ForgotPasswordComponent {
   email: string = '';
 
   constructor(
-    private passwordService:PasswordService
+    private resetMail:ResetMailService,
+    private router: Router,
+    private notificationService: NotificationService
   ) { }
 
   send(){
     const mailData = {
       email:this.email
     }
-    this.passwordService.sendResetMail(mailData)
+    this.resetMail.sendResetMail(mailData).subscribe({
+      next: () => {
+        this.notificationService.showNotification('success', 'Password reset mail sent successfully.');
+        this.router.navigateByUrl('/mail-sent');
+      },
+      error: (err) => {
+        console.error('Error sending reset mail:', err);
+        const msg = err.error?.message || 'Failed to send reset mail.';
+        this.notificationService.showNotification('error', msg);
+      }
+    });
   }
 }
