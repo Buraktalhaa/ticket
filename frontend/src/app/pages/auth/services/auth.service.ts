885286450ff4bff73a393766b950247484efe1d4
Buraktalhaa <talha45879@gmail.com>
2025-06-01 11:47:44 +0300

    auth service changed

--- frontend/src/app/pages/auth/services/auth.service.ts
+++ frontend/src/app/pages/auth/services/auth.service.ts
@@ -9,61 +9,28 @@ import { ApiService } from '../../../shared/services/api.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {  
+export class AuthService {
   public isThereUser = signal(false);
 
   constructor(
     private api: ApiService,
     private router: Router,
     private tokenService: TokenService
-
   ) {
     const hasTokens = this.tokenService.hasValidTokens();
     this.isThereUser.set(hasTokens);
   }
-  
-  signIn(signinData: SignIn): Observable<any> {
-    return this.api
-      .post('http://localhost:3000/auth/sign-in', signinData).pipe(
-        tap((res: HttpResponse<any>) => {
-          this.tokenService.deleteTokens();
-  
-          const accessToken = res.body.accessToken;
-          const refreshToken = res.body.refreshToken;
-  
-          const role = this.tokenService.decodeJwt(accessToken)?.role;
-          this.tokenService.setTokens(accessToken, refreshToken, role);
-  
-          this.isThereUser.set(true);
-        })
-      );
+
+  signIn(signinData: SignIn): Observable<HttpResponse<any>> {
+    return this.api.post('http://localhost:3000/auth/sign-in', signinData);
   }
   
   signUp(signupData: SignUp) {
-    this.api
-      .post('http://localhost:3000/auth/sign-up', signupData)
-      .subscribe((res: HttpResponse<any>) => {
-        this.isThereUser.set(true)
-        this.router.navigateByUrl("/sign-in")
-      });
+    return this.api.post('http://localhost:3000/auth/sign-up', signupData)
   }
 
   signInGoogle() {
-    this.api
-      .get('http://localhost:3000/auth/google')
-      .subscribe((res: HttpResponse<any>) => {
-        const accessToken = res.body.accessToken
-        const refreshToken = res.body.refreshToken
-
-        const role = this.tokenService.decodeJwt(accessToken)?.role;
-
-        this.tokenService.setTokens(accessToken, refreshToken, role);
-        this.isThereUser.set(true);
-        this.isThereUser.set(true)
-        setTimeout(() => {
-          this.router.navigateByUrl('/main');
-        }, 3000);
-      });
+    return this.api.get('http://localhost:3000/auth/google')
   }
 
   logOut() {
