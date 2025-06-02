605bb6a15ab40fa135824f392e2409d5c35c768d
Buraktalhaa <talha45879@gmail.com>
2025-06-02 16:29:38 +0300

    refactor: renamed AdminDashboard to ModeratorDashboard

--- frontend/src/app/pages/moderator/components/moderator-dashboard/moderator-dashboard.component.ts
+++ frontend/src/app/pages/moderator/components/moderator-dashboard/moderator-dashboard.component.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
-import { AdminService } from '../../services/admin.service';
-import { AdminNavigationService } from '../../services/admin-navigation.service';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
 import { FooterInfoTextComponent } from '../../../../shared/components/footer-info-text/footer-info-text.component';
 import { NotificationService } from '../../../../shared/services/notification.service';
+import { ModeratorService } from '../../services/moderator.service';
+import { ModeratorNavigationService } from '../../services/moderator-navigation.service';
 
 @Component({
-  selector: 'app-admin-dashboard',
+  selector: 'app-moderator-dashboard',
   imports: [
     NavbarComponent,
     FooterInfoTextComponent
   ],
-  templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+  templateUrl: './moderator-dashboard.component.html',
+  styleUrl: './moderator-dashboard.component.css'
 })
-export class AdminDashboardComponent {
+export class ModeratorDashboardComponent {
   constructor(
-    private adminService: AdminService,
-    private adminNavigationService: AdminNavigationService,
+    private moderatorService: ModeratorService,
+    private moderatorNavigationService: ModeratorNavigationService,
     private notificationService: NotificationService
   ) { }
 
-  goToAdminStatusPanel() {
-    this.adminService.getAdminStatusPanel().subscribe({
+  goToModeratorStatusPanel() {
+    this.moderatorService.getModeratorStatusPanel().subscribe({
       next: () => {
-        this.adminNavigationService.goToAdminStatusPanel();
+        this.moderatorNavigationService.goToModeratorStatusPanel();
       },
       error: (err) => {
         console.error('Error:', err)
