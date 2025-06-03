233da09c8bf0853255a9764ed7dc73c298490bea
Buraktalhaa <talha45879@gmail.com>
2025-06-03 14:15:49 +0300

    favorite button added

--- frontend/src/app/shared/components/ticket-card/ticket-card.component.ts
+++ frontend/src/app/shared/components/ticket-card/ticket-card.component.ts
@@ -1,30 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { calculateDiscountedPrice } from '../../helpers/discount.helper';
+import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
 
 @Component({
   selector: 'app-ticket-card',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FavoriteButtonComponent
+  ],
   templateUrl: './ticket-card.component.html',
   styleUrl: './ticket-card.component.css'
 })
 export class TicketCardComponent {
-  @Input() description: string =''
-  @Input() stock: number =0
+  @Input() description: string = ''
+  @Input() stock: number = 0
   @Input() buttonText: string = '';
   @Input() price!: number
   @Input() discount!: number
   @Input() imageUrl: string = "/defaultImage.webp";
-  
-  discountedPrice!:number 
+  @Input() ticketId!: string;
   
   @Output() buttonClick = new EventEmitter<void>();
+
+  @Input() isFavorite: boolean = false;
+  @Output() favoriteChanged = new EventEmitter<boolean>();
+
+  discountedPrice!: number
 
   ngOnChanges() {
     this.discountedPrice = calculateDiscountedPrice(this.price, this.discount);
   }
-
   onButtonClick() {
     this.buttonClick.emit();
+  }
+
+  onFavoriteChanged(newValue: boolean) {
+    this.favoriteChanged.emit(newValue);
   }
 }
