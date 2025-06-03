57eff96f64a33f17fe8a7a202bac6be1c116e4b8
Buraktalhaa <talha45879@gmail.com>
2025-06-03 13:42:30 +0300

    feat(favorite): switch to BehaviorSubject for favorites state management

--- frontend/src/app/shared/services/favorite.service.ts
+++ frontend/src/app/shared/services/favorite.service.ts
@@ -1,25 +1,44 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
-
+  private _favorites$ = new BehaviorSubject<string[]>([]);
+  public favorites$ = this._favorites$.asObservable();
+  
   constructor(
     private api: ApiService
   ) { }
 
   getFavorites() {
-    return this.api.get('http://localhost:3000/favorite/get-favorites');
+    this.api.get('http://localhost:3000/favorite/get-favorites').subscribe((res:HttpResponse<any>) => {
+      const favoriteObjs = res.body.data
+      // tum favorileri ekledik
+      const ticketIds = favoriteObjs.map((fav: {ticketId: string}) => fav.ticketId);
+      this._favorites$.next(ticketIds);
+    });
   }
 
   addFavorite(ticketId: string) {
-    console.log(ticketId);
-    return this.api.post('http://localhost:3000/favorite/add-favorite', {ticketId} );
+    this.api.post('http://localhost:3000/favorite/add-favorite', { ticketId }).subscribe(() => {
+      const current = this._favorites$.value;
+      // ticketId yi ekledik
+      this._favorites$.next([...current, ticketId]);
+    });
   }
 
-  removeFavorite(ticketId: string) {
-    return this.api.post(`http://localhost:3000/favorite/delete-favorite`, {ticketId} );
+  deleteFavorite(ticketId: string) {
+    this.api.post('http://localhost:3000/favorite/delete-favorite', { ticketId }).subscribe(() => {
+      const current = this._favorites$.value.filter(id => id !== ticketId);
+      this._favorites$.next(current);
+    });
   }
+
+  isFavorite(ticketId: string): boolean {
+    return this._favorites$.value.includes(ticketId);
+  } 
 }
