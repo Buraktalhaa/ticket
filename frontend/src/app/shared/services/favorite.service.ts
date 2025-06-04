5ed08273c1414aa145921aa576a37c04b6535da1
Buraktalhaa <talha45879@gmail.com>
2025-06-04 14:09:18 +0300

    refactor: remove subscribe calls from FavoriteService methods

--- frontend/src/app/shared/services/favorite.service.ts
+++ frontend/src/app/shared/services/favorite.service.ts
@@ -1,44 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpResponse } from '@angular/common/http';
+import { environment } from '../helpers/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
-  private _favorites$ = new BehaviorSubject<string[]>([]);
-  public favorites$ = this._favorites$.asObservable();
-  
+
   constructor(
     private api: ApiService
   ) { }
 
   getFavorites() {
-    this.api.get('http://localhost:3000/favorite/get-favorites').subscribe((res:HttpResponse<any>) => {
-      const favoriteObjs = res.body.data
-      // tum favorileri ekledik
-      const ticketIds = favoriteObjs.map((fav: {ticketId: string}) => fav.ticketId);
-      this._favorites$.next(ticketIds);
-    });
+    return this.api.get(`${environment.apiUrl}/favorite/get-favorites`)
   }
 
   addFavorite(ticketId: string) {
-    this.api.post('http://localhost:3000/favorite/add-favorite', { ticketId }).subscribe(() => {
-      const current = this._favorites$.value;
-      // ticketId yi ekledik
-      this._favorites$.next([...current, ticketId]);
-    });
+    return this.api.post(`${environment.apiUrl}/favorite/add-favorite`, { ticketId })
   }
 
   deleteFavorite(ticketId: string) {
-    this.api.post('http://localhost:3000/favorite/delete-favorite', { ticketId }).subscribe(() => {
-      const current = this._favorites$.value.filter(id => id !== ticketId);
-      this._favorites$.next(current);
-    });
+    return this.api.post(`${environment.apiUrl}/favorite/delete-favorite`, { ticketId })
   }
-
-  isFavorite(ticketId: string): boolean {
-    return this._favorites$.value.includes(ticketId);
-  } 
 }
