5ee4fd9c89810f7478baf961d07b5602a814ae2d
Buraktalhaa <talha45879@gmail.com>
2025-06-10 15:42:43 +0300

    refactor: favorites component changed

--- frontend/src/app/pages/favorite/components/favorites/favorites.component.ts
+++ frontend/src/app/pages/favorite/components/favorites/favorites.component.ts
@@ -2,29 +2,68 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
 import { FavoriteService } from '../../../../shared/services/favorite.service';
 import { Ticket } from '../../../ticket/types/ticket.types';
-import { HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FilterTicketService } from '../../../../shared/services/filter-ticket.service';
+import { FavoriteCartComponent } from '../../../../shared/components/favorite-cart/favorite-cart.component';
+import { FooterInfoTextComponent } from '../../../../shared/components/footer-info-text/footer-info-text.component';
 
 @Component({
   selector: 'app-favorites',
   imports: [
     NavbarComponent,
-    CommonModule
+    CommonModule,
+    FavoriteCartComponent,
+    FooterInfoTextComponent
   ],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css'
 })
 export class FavoritesComponent {
   tickets: Ticket[] = [];
+  filteredTickets: Ticket[] = [];
 
   constructor(
     private favoriteService: FavoriteService,
+    private filterTicketService: FilterTicketService,
   ) { }
 
   ngOnInit(): void {
+    this.loadFavorites();
+  }
+
+  loadFavorites(): void {
     this.favoriteService.getFavorites().subscribe((res: HttpResponse<any>) => {
-      this.tickets = res.body.data.map((item: any) => item.ticket);
-      console.log(this.tickets);
+      this.tickets = res.body.data.map((item: any) => ({
+        ...item.ticket,
+        isFavorite: item.isFavorite
+      }));
+      this.filteredTickets = [...this.tickets];
     });
+  }
+  
+
+  onFavoriteChanged(ticketId: string, isFav: boolean): void {
+    if (isFav) {
+      this.favoriteService.addFavorite(ticketId).subscribe({
+        next: () => console.log('Added to favorites'),
+        error: (err: HttpErrorResponse) => console.error('Error:', err)
+      });
+
+    } else {
+      this.favoriteService.deleteFavorite(ticketId).subscribe({
+        next: () => {
+          console.log('Removed from favorites');
+
+          this.tickets = this.tickets.filter(ticket => ticket.id !== ticketId);
+          this.filteredTickets = this.filteredTickets.filter(ticket => ticket.id !== ticketId);
+        },
+        error: (err: HttpErrorResponse) => console.error('Error:', err)
+      });
+    }
+  }
+
+  onFilter(filter: { sortBy: string; keyword: string }) {
+    this.filteredTickets = this.filterTicketService.filterTickets(this.tickets, filter);
   }
 }
