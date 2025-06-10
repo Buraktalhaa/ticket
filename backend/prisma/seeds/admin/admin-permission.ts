19e54b3fdfb7dbd71462af65d642ce6a5696312c
Buraktalhaa <talha45879@gmail.com>
2025-06-10 07:39:40 +0300

    admin permission created

---
+++ backend/prisma/seeds/admin/admin-permission.ts
@@ -0,0 +1,8 @@
+import prisma from "../../../src/common/utils/prisma";
+
+export const createAdminPermissions = async () => {
+    
+    return {
+
+    };
+};
--- backend/prisma/seeds/user/user-permission.ts
+++ backend/prisma/seeds/user/user-permission.ts
@@ -3,18 +3,31 @@ import prisma from "../../../src/common/utils/prisma";
 export const createUserPermissions = async () => {
   // User permissions
   const userPermission1 = await prisma.permission.create({ data: { url: '/my-profile' } });
+  
   const userPermission2 = await prisma.permission.create({ data: { url: '/my-profile/edit' } });
+  
   const userPermission3 = await prisma.permission.create({ data: { url: '/get-points' } });
+  
   const userPermission4 = await prisma.permission.create({ data: { url: '/create-order' } });
+  
   const userPermission5 = await prisma.permission.create({ data: { url: '/delete-order' } });
+  
   const userPermission6 = await prisma.permission.create({ data: { url: '/get-orders' } });
+  
   const userPermission7 = await prisma.permission.create({ data: { url: '/cancel-order' } });
+  
   const userPermission8 = await prisma.permission.create({ data: { url: '/get-cart' } });
+  
   const userPermission9 = await prisma.permission.create({ data: { url: '/add-to-cart' } });
+  
   const userPermission10 = await prisma.permission.create({ data: { url: '/update-cart' } });
+  
   const userPermission11 = await prisma.permission.create({ data: { url: '/delete-cart' } });
+  
   const userPermission12 = await prisma.permission.create({ data: { url: '/add-favorite' } });
+  
   const userPermission13 = await prisma.permission.create({ data: { url: '/get-favorites' } });
+  
   const userPermission14 = await prisma.permission.create({ data: { url: '/delete-favorite' } });
 
   return {
